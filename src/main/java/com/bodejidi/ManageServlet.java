1b6ed7b8c80ab55e302bdad44d29ed90aeac1b68
shihang <study1228.sh@gmail.com>
2014-03-13 23:37:58 +0800

    添加了Post方法

--- src/main/java/com/bodejidi/ManageServlet.java
+++ src/main/java/com/bodejidi/ManageServlet.java
@@ -11,4 +11,11 @@ public class ManageServlet extends HttpServlet
     {
         resp.getWriter().println("hello manageservlet");
     }
+    public void doPost(HttpSevletRequest req,HttpServletResponse resp)
+    throws ServletException,IOException
+    {
+        String firstName = req.getParameter("first_name");
+	String lastName = req.getParameter("last_name");
+	resp.getWriter().println(firstName"+"lastName);
+    }
 }
