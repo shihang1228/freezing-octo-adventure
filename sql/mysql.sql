c3f2d087a6db796882a8d2980131b6d5770cda07
shihang <study1228.sh@gmail.com>
2014-03-14 13:06:39 +0800

    新增mysql

---
+++ sql/mysql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS 'member' (
+    'ID' tinyint(4) NOT NULL,
+    'last_name' varchar(16) NOT NULL,
+    'date_created' datetime NOT NULL,
+    'last_updated' datetime NOT NULL,
+    PRIMARY KEY ('ID')
+)   ENGINE=InnoDB DEFAULT CHARSET=utf8 PACK_KEYS=0;
+
+
+hehe
